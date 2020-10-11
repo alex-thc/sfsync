2f6a174b49a7fda6bbf41a547452fab6c7813732
Alex Komyagin <adkomyagin@gmail.com>
2020-10-11 00:01:37 -0700

    Working pipeline

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var jsforce = require('jsforce');
 const tr = require('./transform');
 const Realm = require('realm-web');
 var assert = require('assert');
+const moment = require('moment');
 
 const realmApp = new Realm.App({ id: "shadowforce-sivxs" });
 const realmApiKey = "PDmuuqHPGyRF3eMVWsqle43rcDAaohEdwJIhFWtHivqJ86b3g4crnP8oienz7J7h"
@@ -36,16 +37,38 @@ oauth2.authenticate("psintegration@mongodb.com.stage","cFt67sp11mCiHSxdO3oGYUpxk
   	  console.log("Successfully logged in to Realm!")
 
   	  //projects
-	  conn.query("SELECT Id, Name, SystemModStamp, pse__Is_Active__c, pse__Stage__c FROM pse__Proj__c WHERE pse__Stage__c IN ('In Progress','At Risk') OR SystemModstamp > 2020-07-01T00:00:00.000Z", function(err, result) {
-		  if (err) { return console.error(err); }
-		  console.log("total : " + result.totalSize);
-		  console.log("fetched : " + result.records.length);
-		  let docs = tr.projects_transform(result.records)
-		  user.functions.loadProjects(docs).then(res => {
-		  	  console.log(res)
-		  }).catch((error) => {
-			  console.error("Failed to load projects into Realm", error);
+  	  user.functions.getTimestamp("psproject").then(ts => {
+  	  	  console.log("Project timestamp:",ts)
+  	  	  var cond_where;
+  	  	  if (ts) {
+  	  	  	let date = moment(ts).toISOString();
+  	  	  	cond_where = `SystemModstamp > ${date}`
+  	  	  } else {
+  	  	  	//we need to get anything that expires in this quarter or later
+  	  	  	//for simplicity, we'll just look 3 months back
+  	  	  	let date = moment().subtract(3, 'months').format('YYYY-MM-DD');
+  	  	  	//console.log(date)
+  	  	  	//return
+  	  	  	//cond_where = "pse__Stage__c IN ('In Progress','At Risk') OR (pse__Stage__c = 'Expired' AND )"
+  	  	  	cond_where = `pse__End_Date__c >= ${date}`
+  	  	  }
+
+		  conn.query(`SELECT Id, Name, SystemModStamp, pse__Is_Active__c, pse__Stage__c, pse__End_Date__c FROM pse__Proj__c WHERE ${cond_where}`, function(err, result) {
+			  if (err) { return console.error(err); }
+			  console.log("total : " + result.totalSize);
+			  console.log("fetched : " + result.records.length);
+			  if (result.records.length > 0) {
+				  let docs = tr.projects_transform(result.records)
+				  user.functions.loadProjects(docs).then(res => {
+				  	  console.log(res)
+				  }).catch((error) => {
+					  console.error("Failed to load projects into Realm", error);
+				  });
+			  }
 		  });
+
+	  }).catch((error) => {
+		  console.error("Failed to get timestamp", error);
 	  });
   }).catch((error) => {
 	console.error("Failed to log into Realm", error);
--- transform.js
+++ transform.js
@@ -1,5 +1,8 @@
 function projects_transform(sf_docs) {
 	var mongo_docs = sf_docs
+	for (var i in mongo_docs) {
+		mongo_docs[i]._id = mongo_docs[i].Id
+	}
 	return mongo_docs
 }
 
