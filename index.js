d2eb41613e424c0eeac7e893b409685dc6be1796
Alex Komyagin <adkomyagin@gmail.com>
2020-10-21 16:46:46 -0700

    Move config into a separate file

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const tr = require('./transform');
 const Realm = require('realm-web');
 var assert = require('assert');
 const moment = require('moment');
+var CONFIG = require('./config-sandbox.json');
 
 //TODO: need to trigger the full resync daily (for projects) to deal with joined fields we don't track
 //or do a selective query somehow
 
-const realmApp = new Realm.App({ id: "shadowforce-sivxs" });
-const realmApiKey = "PDmuuqHPGyRF3eMVWsqle43rcDAaohEdwJIhFWtHivqJ86b3g4crnP8oienz7J7h"
+const realmApp = new Realm.App({ id: CONFIG.realmAppId });
+const realmApiKey = CONFIG.realmApiKey
 
 async function loginApiKey(apiKey) {
   // Create an API Key credential
@@ -21,13 +22,13 @@ async function loginApiKey(apiKey) {
 }
 
 var oauth2 = new jsforce.OAuth2({
-	loginUrl : "https://test.salesforce.com",
-	clientId : "3MVG9oZtFCVWuSwNs8.tqlbKN7l9ZxeM2bBHBoFKYJ_NMHtAIe16LjwOpq4sC.9QgwK5ti_SoVzoQ8l_K2VJ7",
-	clientSecret : "D034CFCE536AB4B1A062C9BD3ABA95CBBEBD406734A7006C007B9E4DB76940CD",
+	loginUrl : CONFIG.sfLoginUrl,
+	clientId : CONFIG.sfClientId,
+	clientSecret : CONFIG.sfClientSecret,
 })
 
 
-oauth2.authenticate("psintegration@mongodb.com.stage","cFt67sp11mCiHSxdO3oGYUpxkz5f1XGl1").then((tokenResponse) => {
+oauth2.authenticate(CONFIG.sfUser,CONFIG.sfPasswordWithKey).then((tokenResponse) => {
   console.log("Successfully logged in to Salesforce!");
   console.log(tokenResponse);
 
