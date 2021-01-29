cffcd9e132c3f4e670a0af34d61b909518aa8742
Alex Komyagin <adkomyagin@gmail.com>
2021-01-28 17:38:01 -0800

    New queue-based sync WIP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var jsforce = require('jsforce');
 const Realm = require('realm-web');
-const loader = require('./loader');
+const sync = require('./sync');
 var assert = require('assert');
 var CONFIG = require('./config-prod.json');
 
@@ -26,33 +26,115 @@ var oauth2 = new jsforce.OAuth2({
 	clientSecret : CONFIG.sfClientSecret,
 })
 
+async function request_should_ignore(dbCollection,request) {
+  //check if we should be ignoring this one
+  //last success within an hour
+  let last_success = await sync.getLatestFinishedRequest(dbCollection);
+  if (last_success && last_success.ts.finished
+  	&& ( (request.ts.created <= last_success.ts.finished)
+  	     || ((request.ts.created - last_success.ts.finished) < CONFIG.syncThrottleSuccess_seconds*1000 ))) {
+  	console.log("Found a recent success", last_success.ts.finished);
+  	return true;
+  }
 
-oauth2.authenticate(CONFIG.sfUser,CONFIG.sfPasswordWithKey).then((tokenResponse) => {
-  console.log("Successfully logged in to Salesforce!");
-  console.log(tokenResponse);
+  let last_fail = await sync.getLatestFailedRequest(dbCollection);
+  if (last_fail && last_fail.ts.finished
+  	&& ( (request.ts.created <= last_fail.ts.finished)
+  	     || ((request.ts.created - last_fail.ts.finished) < CONFIG.syncThrottleFailure_seconds*1000 ))) {
+  	console.log("Found a recent failure", last_fail.ts.finished);
+  	return true;
+  }
 
-  var conn = new jsforce.Connection({
-  		instanceUrl : tokenResponse.instance_url,
-  		accessToken : tokenResponse.access_token
-	});
+  // let last_active = await sync.getLatestInProgressRequest(dbCollection);
+  // if (last_active 
+  // 	&& ( (request.ts.created <= last_active.ts.created)
+  // 	     || ((request.ts.created - last_active.ts.created) < CONFIG.syncInProgressTimeout_seconds*1000 ))) {
+  // 	console.log("Found a recent active sync", last_active.ts.created);
+  // 	return true;
+  // }
 
-  loginApiKey(realmApiKey).then(user => {
-  	  console.log("Successfully logged in to Realm!");
+  return false;
+}
 
-  	  (async () => {
-   		  await loader.loadProjects(user,conn);
-   		  await loader.loadMilestones(user,conn);
-   		  await loader.loadSchedules(user,conn);
-   		  await loader.loadOpportunities(user,conn);
-   		  await loader.syncSchedules(user,conn);
-	  })()
+async function process_request(realmUser,dbCollection,request) {
+  console.log(`Processing request ${request._id}`)
+  console.log(request)
 
-	  // user.mongoClient("mongodb-atlas").db("shf").collection("psproject").deleteMany({});
-	  // user.mongoClient("mongodb-atlas").db("shf").collection("schedule").deleteMany({});
+  let should_ignore = await request_should_ignore(dbCollection,request);
+  if (should_ignore) {
+  	await sync.updateRequest(dbCollection,request,{"status":"Ignored"});
+  	return;
+  }
+
+  try {
+
+  	await sync.syncSFChanges(oauth2, CONFIG.sfUser, CONFIG.sfPasswordWithKey, realmUser, dbCollection, request);
+  	await sync.updateRequest(dbCollection,request,{"status":"Finished","ts.finished":new Date()});
+  	console.log("Done");
+
+  } catch(error) {
+
+  	console.error("Failed sync", error);
+  	await sync.updateRequest(dbCollection,request,{"status":"Error","error":error.toString(),"ts.finished":new Date()});
+
+  }
+}
+
+async function workLoop(realmUser,dbCollection) {
+  console.log("Workloop start")
+
+  const query = { "status": "New" };
+  const update = {
+    "$set": {
+      "status": "In Progress",
+      "ts.started" : new Date()
+    }
+  };
+  const options = { returnNewDocument: true, sort: {"ts.created":-1} };
+
+  while(true) {
+    let doc = await dbCollection.findOneAndUpdate(query, update, options);
+    if (!doc)
+      return null;
+    await process_request(realmUser,dbCollection,doc);
+  }
+}
+
+async function watcher(realmUser,dbCollection) {
+    console.log("Watcher start")
+    for await (let event of dbCollection.watch()) {
+        const {clusterTime, operationType, fullDocument} = event;
+
+        if ((operationType === 'insert') && event.fullDocument.status === "New") {
+            const request = event.fullDocument;
+
+            let res = await dbCollection.updateOne({"_id" : request._id, "status": "New"},
+            	{"$set":{"status": "In Progress", "ts.started" : new Date()}});
+            if (res.modifiedCount > 0)
+            {
+              await process_request(realmUser,dbCollection,request);
+            } else {
+              console.log(`Watcher: someone else picked up the request ${request._id}`)
+            }
+        }
+    }
+  }
+
+loginApiKey(realmApiKey).then(user => {
+    console.log("Successfully logged in to Realm!");
+
+    const dbCollection = user
+      .mongoClient('mongodb-atlas')
+      .db('sync')
+      .collection('requests');
+
+    let timerId = setTimeout(async function watchForUpdates() {
+        timerId && clearTimeout(timerId);
+        await workLoop(user,dbCollection);
+        await watcher(user,dbCollection);
+        timerId = setTimeout(watchForUpdates, 5000);
+    }, 5000);
+
   }).catch((error) => {
-	console.error("Failed to log into Realm", error);
+    console.error("Failed to log into Realm", error);
   });
-
-}).catch((error) => {
-  console.error("Failed to log into Salesforce", error);
-});
---
+++ sync.js
@@ -0,0 +1,60 @@
+const loader = require('./loader');
+var jsforce = require('jsforce');
+
+async function updateRequest(dbCollection,request,setter) {
+	return dbCollection.updateOne({"_id" : request._id},
+            	{"$set":setter});
+}
+
+async function getLatestFinishedRequest(dbCollection) {
+	const query = { "status": "Finished" };
+	const options = { sort: {"ts.created":-1} };
+
+	return dbCollection.findOne(query, options);
+}
+
+async function getLatestFailedRequest(dbCollection) {
+	const query = { "status": "Error" };
+	const options = { sort: {"ts.created":-1} };
+
+	return dbCollection.findOne(query, options);
+}
+
+// async function getLatestInProgressRequest(dbCollection) {
+// 	const query = { "status": "In Progress" };
+// 	const options = { sort: {"ts.created":-1} };
+
+// 	return dbCollection.findOne(query, options);
+// }
+
+async function syncSFChanges(oauth2, sfUser, sfPasswordWithKey, realmUser, dbCollection, request) {
+	// var tokenResponse = await oauth2.authenticate(sfUser,sfPasswordWithKey);
+	// console.log("Successfully logged in to Salesforce!");
+	// console.log(tokenResponse);
+
+	// var conn = new jsforce.Connection({
+	// 	instanceUrl : tokenResponse.instance_url,
+	// 	accessToken : tokenResponse.access_token
+	// });
+
+	// await loader.loadProjects(realmUser,conn);
+	// updateRequest(dbCollection,request,{"ts.projects":new Date()});
+
+	// await loader.loadMilestones(realmUser,conn);
+	// updateRequest(dbCollection,request,{"ts.milestones":new Date()});
+
+	// await loader.loadSchedules(realmUser,conn);
+	// updateRequest(dbCollection,request,{"ts.schedules":new Date()});
+
+	// await loader.loadOpportunities(realmUser,conn);
+	// updateRequest(dbCollection,request,{"ts.opportunities":new Date()});
+
+	// await loader.syncSchedules(realmUser,conn);
+	// updateRequest(dbCollection,request,{"ts.schedules_sync":new Date()});
+}
+
+module.exports = { 
+	syncSFChanges, updateRequest,
+	getLatestFinishedRequest, getLatestFailedRequest
+	//, getLatestInProgressRequest
+	}
