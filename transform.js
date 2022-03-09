c6e9800f5e450b85dec6870b411d97ef53ebde65
Alex Komyagin <adkomyagin@gmail.com>
2022-03-09 10:09:09 -0800

    Hack for EM field

--- transform.js
+++ transform.js
@@ -287,6 +287,11 @@ function opportunity_posttransform(doc) {
 	var em_needed = getSpecialTagValue(doc.ps_notes, "%EM_NEEDED%");
 	if (ps_needed || em_needed)
 		doc.ps_triage = {ps_needed, em_needed};
+
+	//hack to make sure the EM subdoc is present until we get this resolved
+	if(!doc.em) {
+		doc.em = {engagement_manager:null}
+	}
 	return doc;
 }
 
